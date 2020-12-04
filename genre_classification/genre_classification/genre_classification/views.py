7f5e841ccd5e6857dad2b74d892e2a8a0758ce81
shabp <shabp@student.chalmers.se>
2020-12-04 20:29:41 +0100

    Implemented model in django app and changed notebook
    The django app now successfully implements the keras model. When a song is uploaded a prediction is made and the genre predicted is prompted to the user.
    
    The jupyter-notebook file now can save the encoder and the scaler as files to then use them to scale the extracted data and make a prediction and then use the label encoder to translate the predction result to labels (genres)

--- genre_classification/genre_classification/genre_classification/views.py
+++ genre_classification/genre_classification/genre_classification/views.py
@@ -1,37 +1,62 @@
 import librosa
+import joblib
 import numpy as np
+import pandas as pd
 from django.shortcuts import render
 from genre_classification.forms import DocumentForm
-
+from genre_classification.extract_features import extract
+from sklearn.preprocessing import LabelEncoder
+from keras import models
+from sklearn import preprocessing
 
 def handle_file_upload(request):
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
         if form.is_valid():
             filename = request.FILES['document']
-            print(filename)
-
+            print("\n*************")
+            print("Processing " + str(filename) + "...")
+            print("*************\n")
             # Feature extraction
-            y, sr = librosa.load(filename, mono=True, duration=30)
-            chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
-            rmse = librosa.feature.rms(y=y)
-            spec_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
-            spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
-            rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
-            zcr = librosa.feature.zero_crossing_rate(y)
-            mfcc = librosa.feature.mfcc(y=y, sr=sr)
+            tuple_data = extract(filename)
 
-            # Building tuple to insert 
-            tuple_data = f'{filename} {np.mean(chroma_stft)} {np.mean(rmse)} {np.mean(spec_cent)} {np.mean(spec_bw)} {np.mean(rolloff)} {np.mean(zcr)}'
-            for e in mfcc:
-                tuple_data += f' {np.mean(e)}'
-            tuple_data = tuple(tuple_data.split())
-            print(tuple_data)
+            # Load ML model from file
+            model = models.load_model("genre_classification/models/model_1")
 
-            # Prediction
-            # prediction = predict(tuple_data)
-            # return HttpResponse(prediction)
-            prediction = "?"
+            # Convert tuple to numpy array
+            track = np.asarray(tuple_data).astype(np.float)
+
+            # Feature number of Columns
+            n_features = track.shape[0]
+
+            # Scale features before prediction by importing scaler  
+            scaler = joblib.load('genre_classification/models/model_1/scaler.bin')
+            track_scaled = scaler.transform(track.reshape(1, -1))
+
+            # Make prediction
+            prediction = model.predict(track_scaled.reshape(-1, n_features))
+            # Take the index of the prediction with highest certainty percentage
+            prediction = np.argmax(prediction[0])            
+            
+            # save np.load
+            np_load_old = np.load
+
+            # modify the default parameters of np.load
+            np.load = lambda *a,**k: np_load_old(*a, allow_pickle=True, **k)
+
+            encoder = LabelEncoder()
+            # Load encoder classes to retrieve the labels
+            encoder.classes_ = np.load('genre_classification/models/model_1/classes.npy')
+            
+            # restore np.load for future normal usage
+            np.load = np_load_old
+            
+            # Inverse transform to get the label
+            prediction = encoder.inverse_transform([prediction])[0]
+
+            print("\n*************")
+            print(filename, "is", prediction)
+            print("*************\n")
 
             return render(request, 'genre_classification/predictions.html', {'form': form, 'prediction': prediction})
     else:
