38f98c9acab5c72f0490a2b2534ce0886bdd34d4
haiderali711 <ali.haider7111@gmail.com>
2020-12-16 21:37:41 +0100

    Solve merge conflict

--- genre_classification/genre_classification/genre_classification/views.py
+++ genre_classification/genre_classification/genre_classification/views.py
@@ -1,3 +1,19 @@
+import os
+import errno
+import shutil
+import sqlite3
+import pandas as pd
+import numpy as np
+import joblib
+from pathlib import Path
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler 
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+from keras import models
+from keras import layers
+from keras import callbacks
 from django.shortcuts import render
 from .forms import DocumentForm
 from .predict import predict
@@ -13,11 +29,16 @@ def handle_file_upload(request):
            
             try:
                  # Feature extraction
-                prediction,label_percentages = predict(filename)
+                prediction,label_percentages,tuple_data = predict(filename)
+
+                print(tuple_data)
 
                 print("\n*************")
                 print(filename, "is", prediction)
                 print("*************\n")
+
+                check_user_data_db()
+
 
                 return render(request, 'genre_classification/predictions.html',
                               {'form': form, 'prediction': prediction , 'label_percentages': label_percentages})
@@ -33,3 +54,41 @@ def handle_file_upload(request):
     })
 
 
+
+
+def check_user_data_db():
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    db_destination = os.path.join(BASE_DIR, 'admins/user_data/user_data.db')
+    try:
+        # Connect / create database
+        con = sqlite3.connect(db_destination)
+
+        # Create table
+        cur = con.cursor()  # instantiate a cursor obj
+        create_table_sql = """
+        CREATE TABLE genrepath (
+        filename text PRIMARY KEY,
+        """
+
+        # Build table
+        header = 'filename '
+        header += 'chroma_stft_mean chroma_stft_var '
+        header += 'rmse_mean rmse_var '
+        header += 'spectral_centroid_mean spectral_centroid_var '
+        header += 'spectral_bandwidth_mean spectral_bandwidth_var '
+        header += 'rolloff_mean rolloff_var '
+        header += 'zero_crossing_rate_mean zero_crossing_rate_var '
+        header += 'harmonics_mean harmonics_var '
+        header += 'perceptual_mean perceptual_var '
+        header += 'tempo '
+        for i in range(1, 21):
+            header += f' mfcc{i}_mean mfcc{i}_var'
+        header += f' genre'
+        header = header.split()
+        for l in header[1:-1]:
+            create_table_sql += l + " real, "
+        create_table_sql += "genre text)"
+
+        cur.execute(create_table_sql)
+    except Exception as e:
+        print(e)
